3be215411506c0207f781764278305a5b0380e9e
Arman Ghoreshi <armangh1377@gmail.com>
2021-03-25 00:14:48 -0400

    Added a demo for JSON schema check, Exam JSON schema check

--- app/exam.py
+++ app/exam.py
@@ -1,13 +1,15 @@
-import functools
 import simplejson as json
+import app.schema as sc
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
+from flask_expects_json import expects_json
 
 bp = Blueprint('exam', __name__, url_prefix='/exam')
 
 
 @bp.route('init', methods=["POST"])
+@expects_json(sc.EXAM_SCHEMA)
 def init_exam():
     print(request.json)
-    return json.dumps({"number": "1", "audio": "/static/index.html"})
+    return json.dumps({"number": "1", "audio": "/PATH/TO/AUDIO/IN/STATIC/DIRECTORY"})
---
+++ app/schema.py
@@ -0,0 +1,59 @@
+# Define JSOn Schemas Here
+
+# Example for an exam JSON that matches wih the EXAM_SCHEMA
+# {
+#   "exam":{
+#     "questions":[
+#       {
+#         "number":"1",
+#         "type":"MC",
+#         "text":"What is 2+2",
+#         "choices":[
+#           {
+#             "letter":"A",
+#             "text":"1"
+#           },
+#           {
+#             "letter":"B",
+#             "text":"4"
+#           }
+#           ],
+#         "answer":null
+#       },
+#       {
+#         "number":"2",
+#         "type":"ES",
+#         "text":"What is Cloud Computing?",
+#         "answer":null
+#       }
+#       ]
+#   }
+# }
+EXAM_SCHEMA = {
+    "type": "object",
+    "properties": {
+        "exam": {
+            "description": "Exam object",
+            "type": "object",
+            "properties": {
+                "questions": {
+                    "description": "Questions in the exam",
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "number": {},
+                            "type": {},
+                            "text": {},
+                            "choices": {},
+                            "answer": {}
+                        },
+                        "required": ["number", "type", "text", "answer"]
+                    }
+                }
+            },
+            "required": ["questions"]
+        }
+    },
+    "required": ["exam"]
+}
