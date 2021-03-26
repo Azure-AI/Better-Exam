784416e85aefeb348acc765a5e5ff43584182dca
Afshin <afshinshah77@gmail.com>
2021-03-26 19:09:24 +0430

    3 methods added - question parser method's body has been written but not tested

--- app/exam.py
+++ app/exam.py
@@ -14,8 +14,25 @@ def init_exam():
     print(request.json)
     return json.dumps({"number": "1", "audio": "/PATH/TO/AUDIO/IN/STATIC/DIRECTORY"})
 
-def text_to_speech(question_type):
+# Parses the questions. Creates an audio file for each of them
+def text_to_speech(questions_json):
+    question_parser(questions_json)
+    long_answer_question_xml()
     pass
 
-def long_answer_question_xml(question_list):
-    return xml_list
+# Gets the long answer questions in the form of a dict. Creates an audio file for each of them
+def long_answer_question_xml(long_questions):
+    questions = json.loads(long_questions)
+    pass
+
+# Gets the whole questions' JSON as input. Seperates the questions based on their type into dicts
+def question_parser(questions_json):
+    questions = json.loads(questions_json)['exam']['questions']
+    es_questions = []
+    ma_questions = []
+    for question in questions:
+        if question['type']=='ES':
+            es_questions.append(question)
+        elif question['type']=='MA':
+            ma_questions.append(question)
+    return es_questions,ma_questions
