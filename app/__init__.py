5fc46a29a4a8a30d7d9d30cb61da60e2ccad9799
Arman Ghoreshi <armangh1377@gmail.com>
2021-03-26 13:19:25 -0400

    updating config file

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,15 @@ import os
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev'
+        SECRET_KEY='',
+        S2T_KEY=''
     )
     app.config.from_pyfile("config.py")
     try:
         os.makedirs(app.instance_path)
     except OSError:
         print("ERR: instance path does not exist \n {}".format(OSError))
+    print(app.config)
 
     @app.route('/', methods=['GET'])
     def hello():
