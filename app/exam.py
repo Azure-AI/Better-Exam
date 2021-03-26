45a084fa9a5c6ec69953a04386af7db2b6fb8b56
Afshin <afshinshah77@gmail.com>
2021-03-26 21:23:48 +0430

    Text-to-Speech service was tested. A test XML was given to the service and the generated WAV file was stored in the audio folder

--- app/exam.py
+++ app/exam.py
@@ -1,5 +1,8 @@
 import simplejson as json
 import app.schema as sc
+from xml.dom import minidom
+from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat
+from azure.cognitiveservices.speech.audio import AudioOutputConfig
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
@@ -11,7 +14,8 @@ bp = Blueprint('exam', __name__, url_prefix='/exam')
 @bp.route('init', methods=["POST"])
 @expects_json(sc.EXAM_SCHEMA)
 def init_exam():
-    print(request.json)
+    # print(request.json)
+    es_question_xml()
     return json.dumps({"number": "1", "audio": "/PATH/TO/AUDIO/IN/STATIC/DIRECTORY"})
 
 # Parses the questions. Creates an audio file for each of them
@@ -27,7 +31,7 @@ def long_answer_question_xml(long_questions):
 
 # Gets the whole questions' JSON as input. Seperates the questions based on their type into dicts
 def question_parser(questions_json):
-    questions = json.loads(questions_json)['exam']['questions']
+    questions = questions_json['exam']['questions']
     es_questions = []
     ma_questions = []
     for question in questions:
@@ -36,3 +40,13 @@ def question_parser(questions_json):
         elif question['type']=='MA':
             ma_questions.append(question)
     return es_questions,ma_questions
+
+def es_question_xml():
+    speech_config = SpeechConfig(endpoint="https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken")
+    speech_config.authorization_token="2a32fa5f2c504b34bd6ba61d496fed6f"
+    synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=None)
+    ssml_string = "<speak version=\"1.0\" xmlns=\"https://www.w3.org/2001/10/synthesis\" xml:lang=\"en-US\"> <voice name=\"en-GB-George-Apollo\"> When you're on the motorway, it's a good idea to use a sat-nav.</voice> </speak>"
+    result = synthesizer.speak_ssml_async(ssml_string).get()
+    
+    stream = AudioDataStream(result)
+    stream.save_to_wav_file("./app/static/audio/test.wav")
