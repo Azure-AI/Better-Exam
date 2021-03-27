01abd5f90c936e70d0b3bd889d1010a2bbfb6c46
Arman Ghoreshi <armangh1377@gmail.com>
2021-03-27 13:46:12 -0400

    Continuous speech recognition works now

--- app/exam.py
+++ app/exam.py
@@ -1,3 +1,4 @@
+import time
 import simplejson as json
 import app.schema as sc
 import os
@@ -49,22 +50,21 @@ def question_parser(questions_json):
 
 
 def es_question_xml():
-
     speak = ET.Element('speak')
     voice = ET.SubElement(speak, 'voice')
-    speak.set('version','1.0')
-    speak.set('xmlns','https://www.w3.org/2001/10/synthesis')
-    speak.set('xml:lang','en-US')
-    voice.set('name',"en-GB-George-Apollo")
+    speak.set('version', '1.0')
+    speak.set('xmlns', 'https://www.w3.org/2001/10/synthesis')
+    speak.set('xml:lang', 'en-US')
+    voice.set('name', "en-GB-George-Apollo")
     voice.text = 'When you\'re on the motorway, it\'s a good idea to use a sat-nav.'
-    question_string = ET.tostring(speak,encoding='unicode', method='xml')
+    question_string = ET.tostring(speak, encoding='unicode', method='xml')
     print(type(question_string))
     speech_config = SpeechConfig(endpoint="https://westeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken")
     speech_config.authorization_token = "2a32fa5f2c504b34bd6ba61d496fed6f"
     synthesizer = SpeechSynthesizer(speech_config=speech_config, audio_config=None)
-    
+
     result = synthesizer.speak_ssml_async(question_string).get()
-    
+
     stream = AudioDataStream(result)
     stream.save_to_wav_file("./app/static/audio/test.wav")
 
@@ -73,19 +73,50 @@ def es_question_xml():
 def answer():
     f = request.files['file']
     f.save(os.path.join(current_app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    time.sleep(0.5)
+    answer_text = speech_recognize_continuous_from_file(os.path.join(current_app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    os.remove(os.path.join(current_app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    return answer_text
+
+
+def speech_recognize_continuous_from_file(filename):
+    """performs continuous speech recognition with input from an audio file"""
+    # <SpeechContinuousRecognitionWithFile>
     speech_config = speechsdk.SpeechConfig(subscription="2a32fa5f2c504b34bd6ba61d496fed6f", region="westeurope")
-    speech_config.speech_recognition_language = "en-US"
-    audio_input = speechsdk.AudioConfig(filename="app/uploads/test1.wav")
-    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_input)
-    result = speech_recognizer.recognize_once_async().get()
-    print(result.text)
-    if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("Recognized: {}".format(result.text))
-    elif result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(result.no_match_details))
-    elif result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-    return result.text
+    audio_config = speechsdk.audio.AudioConfig(filename=filename)
+
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+
+    done = False
+
+    def stop_cb(evt):
+        """callback that signals to stop continuous recognition upon receiving an event `evt`"""
+        print('CLOSING on {}'.format(evt))
+        nonlocal done
+        done = True
+
+    all_results = ""
+
+    def handle_final_result(evt):
+        nonlocal all_results
+        all_results += evt.result.text
+
+    speech_recognizer.recognized.connect(handle_final_result)
+    # Connect callbacks to the events fired by the speech recognizer
+    speech_recognizer.recognizing.connect(lambda evt: print('RECOGNIZING: {}'.format(evt)))
+    speech_recognizer.recognized.connect(lambda evt: print('RECOGNIZED: {}'.format(evt)))
+    speech_recognizer.session_started.connect(lambda evt: print('SESSION STARTED: {}'.format(evt)))
+    speech_recognizer.session_stopped.connect(lambda evt: print('SESSION STOPPED {}'.format(evt)))
+    speech_recognizer.canceled.connect(lambda evt: print('CANCELED {}'.format(evt)))
+    # stop continuous recognition on either session stopped or canceled events
+    speech_recognizer.session_stopped.connect(stop_cb)
+    speech_recognizer.canceled.connect(stop_cb)
+
+    # Start continuous speech recognition
+    speech_recognizer.start_continuous_recognition()
+    while not done:
+        time.sleep(.5)
+
+    speech_recognizer.stop_continuous_recognition()
+    return all_results
+    # </SpeechContinuousRecognitionWithFile>
